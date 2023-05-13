82460da7103410fd298b1ca7af29950cb8feaf68
satomizawazawa <viva3sx@gmail.com>
2023-05-13 10:37:27 +0900

    修正

--- web/mysql_setup.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS DailyReportSystem CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'devuser'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%';
